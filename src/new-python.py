404ab20490bf8357c755774b708f22a79d6527f2
Rohit Thapliyal <rohitthapliyal2000@gmail.com>
2018-01-27 14:20:26 +0530

    driver file

---
+++ src/new-python.py
@@ -0,0 +1,14 @@
+import user_ranks as us
+
+#user input
+handle = input("Enter handle for user 1 : ")
+
+dic1 = {}
+
+us.details_for_one(handle, dic1)
+
+handle = input("Enter handle for user 2 : ")
+
+dic2 = {}
+
+us.details_for_one(handle, dic2)
