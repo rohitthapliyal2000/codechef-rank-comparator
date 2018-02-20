1d5f9ec3908733b3e85982ae69c9555731135ab6
Rohit Thapliyal <rohitthapliyal2000@gmail.com>
2018-02-20 16:47:49 +0530

    Update : Lag-free input

--- src/driver.py
+++ src/driver.py
@@ -19,29 +19,29 @@ print('\nMaximise the terminal\n')
 handle = input("Enter handle for user 1 : ")
 handle1 = handle
 
+# user input 2
+handle = input("Enter handle for user 2 : ")
+handle2 = handle
+
 # dictionary [contest -> rank] for user1
 dic1 = {}
 
 # scraping contests and respective ranks for user1
-us.details_for_one(handle, dic1, lis1)
+us.details_for_one(handle1, dic1, lis1)
 
-rating1 = ur.ratings(handle)
+rating1 = ur.ratings(handle1)
 
-name1 = un.name(handle)
-
-# user input 2
-handle = input("Enter handle for user 2 : ")
-handle2 = handle
+name1 = un.name(handle1)
 
 # dictionary [contest -> rank] for user2
 dic2 = {}
 
 # scraping contests and respective ranks for user2
-us.details_for_one(handle, dic2, lis2)
+us.details_for_one(handle2, dic2, lis2)
 
-rating2 = ur.ratings(handle)
+rating2 = ur.ratings(handle2)
 
-name2 = un.name(handle)
+name2 = un.name(handle2)
 
 print('\n')
 ll.finish_off(handle1, handle2, name1, name2, rating1, rating2, dic1, dic2, lis1, lis2)
