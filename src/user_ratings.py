76cfbf82828a2ba04efe7720765ec206763fa85b
Neeraj Negi <neeraj.negi174@gmail.com>
2018-02-23 14:45:45 +0530

    Fetching codechef ratings

--- src/user_ratings.py
+++ src/user_ratings.py
@@ -1,7 +1,11 @@
 import requests
 from lxml import html
+import sys
+import lxml.etree
+import lxml._elementpath
 
-def ratings(handle):
+
+def ratings(handle, result):
 
 	str = ""
 
@@ -15,10 +19,12 @@ def ratings(handle):
 	data = tree.xpath("//div/section/div/div/div/a/text()")
 
 	if len(data) == 0:
-		print("Not a valid handle")
+		result = result + "Handle: " + handle + " is invalid."
+		print("Wrong Handle: user_ratings_py")
+		sys.exit()
 
 	else:
 		for i in range(0, len(data[0])-2):
 			str += data[0][i]
 
-	return str
+	return str2
